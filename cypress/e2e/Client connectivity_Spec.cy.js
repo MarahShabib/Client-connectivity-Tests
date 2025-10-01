a3febaf89c211787157038f1b5bea118212f6d35
Marah Shabib (EXT-Nokia) <marah.shabib.ext@nokia.com>
2025-10-01 15:39:01 +0300

    Update Test

--- cypress/e2e/Client connectivity_Spec.cy.js
+++ cypress/e2e/Client connectivity_Spec.cy.js
@@ -6,48 +6,41 @@ describe('Client connectivity Policy Tests', () => {
     loginAndNavigateToAccessPolicy()
   })
 
-  // it('Access Policy UI is rendered correctly', () => {
-  //   assertAccessPolicyUi()
-  // })
-
-  // it('Actions menu contains expected items', () => {
-  //   assertActionsMenuItems(MENU_ITEMS)
-  // })
 
 
-  // it('New Rule form should display the required fields and controls', () => {
-  //   assertNewRuleFormUi()
-  // })
+  it('New Rule form should display the required fields and controls', () => {
+    assertNewRuleFormUi()
+  })
 
 
-  // it('Create New Rule', () => {
-  //   createRule(variables.RULE_BASIC_LAST)
-  //   checkSuccessMessage(msgs.CREATE_SUCCESS)
-  //   publishRule()
-  //   checkSuccessMessage(msgs.PUBLISH_SUCCESS)
-  // })
+  it('Create New Rule', () => {
+    createRule(variables.RULE_BASIC_LAST)
+    checkSuccessMessage(msgs.CREATE_SUCCESS)
+    publishRule()
+    checkSuccessMessage(msgs.PUBLISH_SUCCESS)
+  })
 
-  // it('Create New Rule with Country', () => {
-  //   createRule(variables.RULE_WITH_COUNTRY)
-  //    checkSuccessMessage(msgs.CREATE_SUCCESS)
-  //    publishRule()
-  //    checkSuccessMessage(msgs.PUBLISH_SUCCESS)
-  // })
+  it('Create New Rule with Country', () => {
+    createRule(variables.RULE_WITH_COUNTRY)
+     checkSuccessMessage(msgs.CREATE_SUCCESS)
+     publishRule()
+     checkSuccessMessage(msgs.PUBLISH_SUCCESS)
+  })
 
-  // it('Create New Rule with Action', () => {
-  //   createRule(variables.RULE_WITH_ACTION)
-  //    checkSuccessMessage(msgs.CREATE_SUCCESS)
-  //    publishRule()
-  //    checkSuccessMessage(msgs.PUBLISH_SUCCESS)
+  it('Create New Rule with Action', () => {
+    createRule(variables.RULE_WITH_ACTION)
+     checkSuccessMessage(msgs.CREATE_SUCCESS)
+     publishRule()
+     checkSuccessMessage(msgs.PUBLISH_SUCCESS)
 
-  // })
+  })
 
-  // it('Create New Rule with Position = First', () => {
-  //   createRule(variables.RULE_POSITION_FIRST)
-  //   checkSuccessMessage(msgs.CREATE_SUCCESS)
-  //   publishRule()
-  //   checkSuccessMessage(msgs.PUBLISH_SUCCESS)
-  // })
+  it('Create New Rule with Position = First', () => {
+    createRule(variables.RULE_POSITION_FIRST)
+    checkSuccessMessage(msgs.CREATE_SUCCESS)
+    publishRule()
+    checkSuccessMessage(msgs.PUBLISH_SUCCESS)
+  })
 
   it('should filter the rule list based on search input', () => {
     filterRulesBySearch(variables.SearchRule)
